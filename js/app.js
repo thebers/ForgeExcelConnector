36ebf8686193775be4c3c6b452472cf7b630ebc1
Ben Bringardner <ben.bringardner@gmail.com>
2018-10-17 23:52:23 -0500

    Testing Ideas

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ $(document).ready (function () {
 // the following is from https://github.com/Autodesk-Forge/forge-api-nodejs-client and requires Nodejs...which I just learned doesnt belong here!
 
   $('#grabToken').click(function(){
-    var ForgeSDK = require('forge-apis');
-    var CLIENT_ID = 'idL3OtY8Oy7vc7IWCYXxlLAMAyP7fz9E' , CLIENT_SECRET = 'Iee8358d303b14d1';
+    var CLIENT_ID = 'idL3OtY8Oy7vc7IWCYXxlLAMAyP7fz9E';
+    var CLIENT_SECRET = 'Iee8358d303b14d1';
 
     // Initialize the 2-legged OAuth2 client, set specific scopes and optionally set the `autoRefresh` parameter to true
     // if you want the token to auto refresh
     var autoRefresh = true; // or false
+    console.log(CLIENT_ID);
+    console.log(CLIENT_SECRET);
 
     var oAuth2TwoLegged = new ForgeSDK.AuthClientTwoLegged(CLIENT_ID, CLIENT_SECRET, [
       'data:read',
@@ -24,5 +26,19 @@ $(document).ready (function () {
     }, function(err){
       console.error(err);
     });
+  });
+
+  $('#ajaxTest').click(function(){
+    var printThis = 'junk snap!';
+    console.log(printThis);
+
+    const Http = new XMLHttpRequest();
+    const url='https://developer.api.autodesk.com/authentication/v1/authenticate';
+    Http.open('POST', url);
+    Http.send();
+    Http.onreadystatechange=(e)=>{
+      console.log(Http.responseText)
+    };
+
   });
 });
